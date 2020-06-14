fc1a4351ea57157eacc9491f932344bcc0ea3b94
unknown <rahulkumar32034@gmail.com>
2020-06-14 22:38:10 +0530

    program1

---
+++ q1.java
@@ -0,0 +1,15 @@
+public class q1{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+        String s1="24.5";
+        String s2="67.9";
+        //String s3;
+        //s3=(String)s1+s2;
+        
+        Double i=Double.parseDouble(s1);
+        Double j=Double.parseDouble(s2);
+        Double s3=i*j;
+        System.out.println(s3);
+     }
+}
