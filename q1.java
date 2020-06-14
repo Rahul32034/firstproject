8932a589898d0edf410b594a92c47126d766e947
Rahul32034 <66233807+Rahul32034@users.noreply.github.com>
2020-06-14 22:41:59 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -2,7 +2,7 @@ public class q1{
 
      public static void main(String []args){
         System.out.println("Hello World");
-        String s1="24.5";
+        String s1="24.50";
         String s2="67.9";
         //String s3;
         //s3=(String)s1+s2;
